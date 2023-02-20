b6cdeb33333a98a0eb8298f9f807383964c9ae17
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-02-19 23:58:26 -0500

    comment

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -72,7 +72,7 @@ export default function Navigation () {
                     />
                 </div>
             </nav>
-            {/* co */}
+            {/* c */}
         </header>
     )
 }
